c9b82d6d216523131dc19c907af1dadffb9f1868
tgdwyer <tgdwyer@gmail.com>
2019-11-09 08:09:41 +1100

    2 files modified

--- index.ts
+++ index.ts
@@ -4,26 +4,42 @@ import { map,filter,flatMap,takeUntil } from 'rxjs/operators';
 function asteroids() {
     const ship = document.getElementById("ship")!;
     const state = {
-        x:50,y:50,angle:0
+        x:75,y:75,angle:0
     }
     fromEvent<KeyboardEvent>(document,'keydown')
       .pipe(
-        filter(keyEvent=>!keyEvent.repeat),
+        filter(({repeat})=>!repeat),
+        filter(({keyCode:k})=>k===37 || k===39),
         flatMap(({keyCode:downKeyCode})=>interval(10).pipe(
-          takeUntil(fromEvent<KeyboardEvent>(document,'keyup').pipe(
-            filter(up=>up.keyCode === downKeyCode))),
-            map(_=>({
-                left:downKeyCode===37,
-                up:downKeyCode===38,
-                right:downKeyCode===39
-            }))
-        )),
-        filter(e=>e.left || e.right),
-        map(e=>({angle:e.left?-1:1}))
+          takeUntil(
+            fromEvent<KeyboardEvent>(document,'keyup').pipe(
+              filter(up=>up.keyCode === downKeyCode))),
+          map(_=>downKeyCode===37?-1:1))
+        )
       ).subscribe({next:e=>{
         ship.setAttribute('transform',
-          `translate(${state.x},${state.y}) rotate(${state.angle+=e.angle})`)
+          `translate(${state.x},${state.y}) rotate(${state.angle+=e})`)
       }})
+}
+
+function asteroidsEvents() {
+    const ship = document.getElementById("ship")!;
+    const state = {
+        x:75,y:75,angle:0
+    }
+    document.onkeydown = e=>{
+      if((e.keyCode === 37 || e.keyCode === 39) && !e.repeat) {
+        const handle = setInterval(i=>{
+        ship.setAttribute('transform',
+          `translate(${state.x},${state.y}) rotate(${state.angle+=e.keyCode === 37 ? -1 : 1})`)
+        },10)
+        document.onkeyup = u=>{
+          if(u.keyCode === e.keyCode) {
+            clearInterval(handle);
+          }
+        }
+      }
+    }
 }
 
 setTimeout(asteroids, 0)
